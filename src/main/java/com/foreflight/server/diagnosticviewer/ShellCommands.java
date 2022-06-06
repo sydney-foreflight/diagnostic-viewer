5a7d1278f9a94c560a6846332277151ac7c866b9
sydney <sydney@foreflight.com>
2022-06-06 15:30:38 -0500

    Getting simple setZipPath and setTargetDirectory as well as unzip on the command line.

--- src/main/java/com/foreflight/server/diagnosticviewer/ShellCommands.java
+++ src/main/java/com/foreflight/server/diagnosticviewer/ShellCommands.java
@@ -3,11 +3,34 @@ package com.foreflight.server.diagnosticviewer;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
+import java.io.IOException;
+
 @ShellComponent
 public class ShellCommands {
 
-    @ShellMethod("test")
-    public String test() {
-        return "yay!";
+    String zipFilePath = "";
+    String saveFilePath = "";
+    FileProcessor reader;
+
+    @ShellMethod("Set zip file directory")
+    public String setZip(String s) {
+        zipFilePath = s;
+        return "Zip file path set to " + s;
+    }
+
+    @ShellMethod("Set save directory")
+    public String setNewDir(String s) {
+        saveFilePath = s;
+        return "Unzipped files will be saved to " + s;
+    }
+
+    @ShellMethod("unzip")
+    public String unzip() throws IOException {
+        if(zipFilePath.length() < 1) {
+            return "Please set path to zip file";
+        } if(saveFilePath.length() < 1) {
+            return "Please set a path to a new directory";
+        } reader = new FileProcessor(zipFilePath, saveFilePath);
+        return reader.getAllFiles();
     }
 }
